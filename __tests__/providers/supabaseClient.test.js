33f4ad0a9adc786f5b9198b237b144d9f546296a
Joritz <echenique.149324@e.unavarra.es>
2026-07-28 19:55:07 +0200

    [] - test supabase verde

--- __tests__/providers/supabaseClient.test.js
+++ __tests__/providers/supabaseClient.test.js
@@ -5,7 +5,7 @@ describe('Supabase Connection', () => {
   test('debe conectarse correctamente a Supabase', async () => {
 
     const { error } = await supabase
-      .from('articulos')
+      .from('maestroArticulo')
       .select('item')
       .limit(1);
 
